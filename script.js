a855697a6b877567d74c76b5f846d9529afc20f5
tanviranik004 <tanvir.anik.009@gmail.com>
2022-06-30 13:58:38 +0600

    dynamic-card

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
 
 //dummy data
-const posts =[
-    {
-        title: 'this is title-1',
-        body: 'this is body-1',
-    },
+// const posts =[
+//     {
+//         title: 'this is title-1',
+//         body: 'this is body-1',
+//     },
 
-    {
-        title: 'this is title-2',
-        body: 'this is body-2',
-    },
+//     {
+//         title: 'this is title-2',
+//         body: 'this is body-2',
+//     },
 
-    {
-        title: 'this is title-3',
-        body: 'this is body-3',
-    },
+//     {
+//         title: 'this is title-3',
+//         body: 'this is body-3',
+//     },
 
-    {
-        title: 'this is title-4',
-        body: 'this is body-4',
-    },
+//     {
+//         title: 'this is title-4',
+//         body: 'this is body-4',
+//     },
 
-    {
-        title: 'this is title-5',
-        body: 'this is body-5',
-    },
+//     {
+//         title: 'this is title-5',
+//         body: 'this is body-5',
+//     },
     
-    {
-        title: 'this is title-6',
-        body: 'this is body-6',
-    },
-    {
-        title: 'this is title-7',
-        body: 'this is body-7',
-    },
-    {
-        title: 'this is title-8',
-        body: 'this is body-8',
-    }
-];
+//     {
+//         title: 'this is title-6',
+//         body: 'this is body-6',
+//     },
+//     {
+//         title: 'this is title-7',
+//         body: 'this is body-7',
+//     },
+//     {
+//         title: 'this is title-8',
+//         body: 'this is body-8',
+//     }
+// ];
 
 //  <div class="post">
 // <h4 class="post-title">post title</h4>
 // <p class="post-body">post description-1</p>
 // </div> 
 
+//fetch data 
+//console.log(axios);
+const fetchData = async (config) => {
+    try{
+        const res = await axios(config);
+          //console.log(res.data);
+          return res.data;
+    }catch(e){
+        throw Error("data is not fetched");
+    }
+   
+};
+
+
 //selection parts
 
 const postsElement = document.querySelector(".posts");
 
-const loadAllData = () =>{
+const loadAllData = async () =>{
+    const posts = await fetchData ("https://jsonplaceholder.typicode.com/posts");
     posts.map((post)=>{
         //console.log(post);
         const postElement = document.createElement("div");
